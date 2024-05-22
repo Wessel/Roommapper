51af99f550d430ae9cb7b63ddf7113443d4eb0ab
zwarenelle <tomie121@gmail.com>
2024-05-22 15:48:43 +0200

    Move map to robotcontroller

---
+++ src/Client/RobotController/Map.cs
@@ -0,0 +1,15 @@
+namespace Mapper
+{
+  public class Map
+  {
+    private Coordinate bottomLeft;
+    private Coordinate topRight;
+
+    public Map(Coordinate bottomLeft, Coordinate topRight)
+    {
+      this.bottomLeft = bottomLeft;
+      this.topRight = topRight;
+    }
+
+  }
+}
